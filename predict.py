345ecf95867f61ea3a231fbfd857eba96669285f
jiriroz <jiri.roznovjak@gmail.com>
2017-09-27 18:31:19 -0400

    Define boilerplate for prediction

---
+++ predict.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+from header import *
+
+class Predictor(object):
+
+    def __init__(self):
+        pass
+
+    def evaluate(self, fname):
+        """
+        @param fname csv filename containing flight data
+        Evaluate our prediction on this dataset.
+        @return an error measure (TBD).
+        """
+        pass
+
+    def predict(self, fname):
+        """
+        @param fname csv filename containing flight data
+        Compute expected delay for each flight.
+        """
+        delays = []
+        with open(fname, "r") as csvfile:
+            reader = csv.reader(csvfile)
+            i = -1
+            for row in reader:
+                if i == -1:
+                    i = 0
+                    continue
+                i += 1
+                delay = compDelayForFlight(row)
+                delays.append(delay)
+        delays = np.array(delay)
+        return delays
+
+    def compDelayForFlight(self, flightData):
+        carrier = self.compCarrierDelay(flightData)
+        weather = self.compExtrWeatherDelay(flightData)
+        nas = self.compNasDelay(flightData)
+        security = self.compSecurityDelay(flightData)
+        late = self.compLateAircraftDelay(flightData)
+        return carrier + weather + nas + security + late
+       
+    def compCarrierDelay(self, flightData):
+        return 0.0
+
+    def compExtrWeatherDelay(self, flightData):
+        return 0.0
+
+    def compNasDelay(self, flightData):
+        return 0.0
+
+    def compSecurityDelay(self, flightData):
+        return 0.0
+
+    def compLateAircraftDelay(self, flightData):
+        return 0.0
+
+class Model(object):
+    def __init__(self, name, index, features):
+        self.index = index
+        self.name = name
+        self.features = features
+
+    def trainLinearModel(self, csvFile, nExamples):
+
+        n = 0
+        for row in iterData(csvFile):
+            if n >= nExamples:
+                break
+            n += 1
+
+
+def iterData(fname):
+    #Helper generator to read from csv files
+    with open(fname, "r") as csvfile:
+        reader = csv.reader(csvfile)
+        first = True
+        for row in reader:
+            if first:
+                first = False
+                continue
+            yield row
+
+
+
+
