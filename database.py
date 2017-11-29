ff4028b4eac9665011debb310a746bde37ee7255
jiriroz <jiri.roznovjak@gmail.com>
2017-11-29 00:52:41 -0500

    Add field prev flight to the db

--- database.py
+++ database.py
@@ -24,10 +24,11 @@ CREATE = '''CREATE TABLE {}
              WEATHER_DELAY REAL,
              NAS_DELAY REAL, 
              SECURITY_DELAY REAL,
-             LATE_AIRCRAFT_DELAY REAL);'''
+             LATE_AIRCRAFT_DELAY REAL)
+             PREV_FLIGHT INTEGER);'''
 
-INSERT = '''INSERT INTO {} (ID, FL_NUM, AIRLINE_ID, DEST_AIRPORT_ID, DISTANCE, YEAR, MONTH, DAY_OF_MONTH, DAY_OF_WEEK, CRS_DEP_TIME, CRS_ARR_TIME, DEP_TIME, ARR_TIME, CRS_ELAPSED_TIME, DEP_DELAY, ARR_DELAY, CARRIER_DELAY, WEATHER_DELAY, NAS_DELAY, SECURITY_DELAY, LATE_AIRCRAFT_DELAY) 
-            VALUES ({}, "{}", {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {});'''
+INSERT = '''INSERT INTO {} (ID, FL_NUM, AIRLINE_ID, DEST_AIRPORT_ID, DISTANCE, YEAR, MONTH, DAY_OF_MONTH, DAY_OF_WEEK, CRS_DEP_TIME, CRS_ARR_TIME, DEP_TIME, ARR_TIME, CRS_ELAPSED_TIME, DEP_DELAY, ARR_DELAY, CARRIER_DELAY, WEATHER_DELAY, NAS_DELAY, SECURITY_DELAY, LATE_AIRCRAFT_DELAY, PREV_FLIGHT) 
+            VALUES ({}, "{}", {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {});'''
 
 tables = dict()
 
@@ -39,6 +40,7 @@ with open("data/all.csv.sorted", "r") as f:
     reader = csv.reader(f)
     first = True
     for row in reader:
+        prev = None
         if first:
             first = False
             continue
@@ -49,14 +51,21 @@ with open("data/all.csv.sorted", "r") as f:
             conn.execute(CREATE.format(tblName))
         for feat in INT_FEATURES:
             row[feat] = int(float(row[feat]))
-        cmd = INSERT.format(tblName, tables[tblName], getFlNum(row), int(row[AIRLINE_ID]),
+
+        flightNum = getFlNum(row)
+        prevFlight = -1
+        if flightNum == prev:
+            prevFlight = tables[tblName] - 1
+
+        cmd = INSERT.format(tblName, tables[tblName], flightNum, int(row[AIRLINE_ID]),
               int(row[DEST_AIRPORT_ID]), int(row[DISTANCE]), int(row[YEAR]), 
               int(row[MONTH]), int(row[DAY_OF_MONTH]), int(row[DAY_OF_WEEK]),
               int(row[CRS_DEP_TIME]), int(row[CRS_ARR_TIME]), int(row[DEP_TIME]),
               int(row[ARR_TIME]), int(row[CRS_ELAPSED_TIME]), float(row[DEP_DELAY]),
               float(row[ARR_DELAY]), float(row[CARRIER_DELAY]),
               float(row[WEATHER_DELAY]), float(row[NAS_DELAY]),
-              float(row[SECURITY_DELAY]), float(row[LATE_AIRCRAFT_DELAY]))
+              float(row[SECURITY_DELAY]), float(row[LATE_AIRCRAFT_DELAY]),
+              prevFlight)
         try:
             conn.execute(cmd)
         except Exception as e:
@@ -65,6 +74,7 @@ with open("data/all.csv.sorted", "r") as f:
             errors += 1
             continue
         tables[tblName] += 1
+        prev = flightNum
 
 conn.commit()
 conn.close()
