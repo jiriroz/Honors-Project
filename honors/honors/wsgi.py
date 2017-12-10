b06e4ee9581d984054670e409571178d8f69b047
jiriroz <jiri.roznovjak@gmail.com>
2017-12-09 23:15:28 -0500

    Set up django app

--- honors/honors/wsgi.py
+++ honors/honors/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for delaysapp project.
+WSGI config for honors project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "delaysapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "honors.settings")
 
 application = get_wsgi_application()
